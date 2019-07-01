184b22706efff3b4e8f6ceeb55a6e5929a6e473d
Agustín Formoso <aformoso@ripe.net>
2019-07-01 13:45:10 +0200

    Date field

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from django.db.models import Model
+from django.db.models import Model, DateTimeField
 from django.contrib.postgres.fields import JSONField
-
-# Create your models here.
+from datetime import datetime
 
 
 class Result(Model):
 
     json = JSONField(default=dict)
+    date = DateTimeField(default=datetime.now)
