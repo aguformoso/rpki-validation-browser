aee99deecd0f03dbbcfadbee8cc5c18c9026d665
Agustín Formoso <aformoso@ripe.net>
2019-08-28 09:27:08 -0300

    Code refactoring Simple test cases Extended Mattermost notification message

--- app/models.py
+++ app/models.py
@@ -1,12 +1,85 @@
-from django.db.models import Model, DateTimeField
+from django.db.models import Model, Manager, DateTimeField
 from django.contrib.postgres.fields import JSONField
 from datetime import datetime
 
 
-class Result(Model):
+class ResultManager(Manager):
 
+    def results_seen(self, signal):
+        return self.filter(
+            json__contains=signal,
+        )
+
+    def results_seen_not_doing_rov(self):
+        return self.results_seen(Result.not_rov_signal)
+
+    def results_seen_doing_rov(self):
+        return self.results_seen(Result.rov_signal)
+
+    def ases_have_been_seen_not_doing_rov(self, asns):
+        return self.results_seen_not_doing_rov(
+        ).filter(
+            json__contains={"asns": asns}
+        ).count() >= 1
+
+    def ases_have_been_seen_doing_rov(self, asns):
+        return self.results_seen_doing_rov(
+        ).filter(
+            json__contains={"asns": asns}
+        ).count() >= 1
+
+    def ases_are_new_to_rov(self, asns):
+        """
+        This method is to be called immediately after saving the object into DB
+        :param asns:
+        :return:
+        """
+        return self.results_seen_doing_rov(
+        ).filter(
+            json__contains={"asns": asns}
+        ).count() == 1
+
+    @staticmethod
+    def is_documentation_asn(asn):
+        """
+
+        Documentation ASNs are used for testing the production API
+
+        :param asn:
+        :return:
+
+        https://tools.ietf.org/html/rfc5398#section-4
+        """
+        return 64496 <= int(asn) <= 64511 or 65536 <= int(asn) <= 65551
+
+
+class Result(Model):
     json = JSONField(default=dict)
     date = DateTimeField(default=datetime.now)
+
+    # Signal that this fetch is being performed
+    # from a network which is doing Route Origin Validation
+    # (rpki-valid=true, rpki-invalid=false, asns)
+    rov_signal = {
+        "rpki-valid-passed": True,
+        "rpki-invalid-passed": False,
+    }
+
+    # Signal that this fetch is being performed
+    # from a network which is *not* doing Route Origin Validation
+    # (rpki-valid=true, rpki-invalid=false, asns)
+    not_rov_signal = {
+        "rpki-valid-passed": True,
+        "rpki-invalid-passed": True,
+    }
+
+    objects = ResultManager()
+
+    def __str__(self):
+        return "(AS{asns}) ROV={rov}".format(
+            asns=self.json['asns'],
+            rov=self.is_doing_rpki()
+        )
 
     def is_doing_rpki(self):
         return self.json["rpki-valid-passed"] and not self.json["rpki-invalid-passed"]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rest_framework import viewsets
 from .serializers import ResultSerializer
 from .models import Result
@@ -17,31 +18,19 @@ class ResultView(viewsets.ModelViewSet):
 
         super(ResultView, self).perform_create(serializer)
 
-        # check if this is a new (rpki-valid=true, rpki-invalid=false, asns) tuple
-        signal = {
-            "rpki-valid-passed": True,
-            "rpki-invalid-passed": False,
-        }
-
         asns = serializer.instance.json['asns']
         pfx = serializer.instance.json['pfx']
 
-        # there's a new AS doing RPKI if the new Result is_doing_rpki=true and there's
+        # We're seeing this AS doing RPKI for the first time if
+        # the new Result is_doing_rpki=true and there's
         # only 1 object in db (this one, has just been saved)
 
-        new = serializer.instance.is_doing_rpki() and Result.objects.filter(
-            json__contains=signal,
-        ).filter(
-            json__contains={"asns": asns}
-        ).count() == 1
+        new = serializer.instance.is_doing_rpki() and Result.objects.ases_are_new_to_rov(asns)
 
         if new:
 
-            documentation_asn = []
             names = []
             for asn in asns.split(','):
-
-                documentation_asn.append(64496 <= int(asn) <= 64511 or 65536 <= int(asn) <= 65551)
 
                 holder = RipestatClient().fetch_info(resource="AS{asn}".format(asn=asn))['data']['holder']
                 names.append(
@@ -57,12 +46,25 @@ class ResultView(viewsets.ModelViewSet):
                 pfx="[{pfx}](https://stat.ripe.net/{pfx})".format(pfx=pfx)
             )
 
+            # We've previously seen this ASNs not doing ROV
+            if Result.objects.ases_have_been_seen_not_doing_rov(asns):
+
+                last_result = Result.objects.results_seen_not_doing_rov(
+                    ).filter(
+                        json__contains={"asns": asns}
+                    ).order_by('date').last()
+
+                msg += ", and we saw {subject} previously not doing Route Origin Validation (last seen: {last_seen}).".format(
+                    subject='them' if len(names) > 1 else 'it',
+                    last_seen=last_result.date.strftime("%b %d %Y %H:%M:%S")
+                )
+
             MattermostClient().send_msg(msg=msg)
 
-            # If any of the ASNs is part of the documentation range, then
-            # don't persist, as it'll be used for testing
-            # https://tools.ietf.org/html/rfc5398#section-4
-            if any(documentation_asn):
-                serializer.instance.delete()
+        # If any of the ASNs is part of the documentation range, then
+        # don't persist, as it'll be used for testing
+        # https://tools.ietf.org/html/rfc5398#section-4
+        if any([Result.objects.is_documentation_asn(asn) for asn in asns.split(',')]):
+            serializer.instance.delete()
 
 
